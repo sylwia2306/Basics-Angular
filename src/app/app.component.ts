01f48cd2f9754f3303d106d76c5f02f196ab5ebf
Sylwia <sylwiaszymanska2306@gmail.com>
2025-01-25 18:44:53 +0100

    basic-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'basics-angular';
+  title: string = 'basics-angular';
+  name: string = 'Karolek';
+  age: number = 2;
+  isAdult: boolean = false;
+
+  constructor() {
+    console.log("KONSTRUKTOR");
+    this.checkAdult();
+  }
+
+  checkAdult() {
+    console.log("metoda");
+    this.isAdult = this.age > 18;
+  }
+
+  // standardowa funkcja
+  xxx(): boolean {
+    return true;
+  }
 }
